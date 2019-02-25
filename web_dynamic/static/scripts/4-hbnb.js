8496e7d5b912356b30c3195a852d7ca9bb501344
jsjimenez51 <jsjimenez51@gmail.com>
2019-02-25 09:40:01 -0800

    Upload Josh's test branch

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,86 @@
+let selectedAmens = [];
+
+$('document').ready(function () {
+  // functionality for Amenity Selection Popover
+  $('.amenities input').change(function () {
+    if ($(this).is(':checked')) {
+      selectedAmens.push($(this).attr('data-name'));
+    } else {
+      let rmAmen = $(this).attr('data-name');
+      let amenIdx = selectedAmens.indexOf(rmAmen);
+      if (amenIdx > -1) {
+        selectedAmens.splice(amenIdx, 1);
+      }
+    }
+    selectedAmens.sort();
+    $('.amenities h4').text(selectedAmens.join(', '));
+  });
+
+  // funtionality for API Status Indicator
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (response) {
+    if (response.status === 'OK') {
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
+
+  // functionality to load places from the front-end
+  $.ajax({
+    type: 'POST',
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    contentType: 'application/json',
+    dataType: 'json',
+    data: '{}',
+    success: apiData
+  });
+
+  // funtionality for the Search Button
+  $('button').click(function () {
+    $('SECTION.places article').remove();
+    $.ajax({
+      type: 'POST',
+      url: 'http://0.0.0.0:5001/api/v1/places_search/',
+      contentType: 'application/json',
+      dataType: 'json',
+      data: JSON.stringify({ amenities: selectedAmens }),
+      success: (data) => {
+        console.log(`Searching for: { "amenities": [${selectedAmens}] }`);
+        apiData(data);
+        console.log("Data", data);
+      }
+    });
+  });
+
+  // format for appended Place data
+  function apiData (response) {
+    for (let place of response) {
+      $('.places').append(
+        `<article>
+          <div class="title">
+            <h2>${place.name}</h2>
+            <div class="price_by_night">
+              ${place.price_by_night}
+            </div>
+          </div>
+          <div class="information">
+            <div class="max_guest">
+              <i class="fa fa-users fa-3x" aria-hidden="true"></i>
+              <br />
+              ${place.number_rooms} Bedrooms
+            </div>
+            <div class="number_rooms">
+              <i class="fa fa-users fa-3x" aria-hidden="true"></i>
+              <br />
+              ${place.number_bathrooms} Bathroom
+            </div>
+          </div>
+          </div class="user">
+          </div>
+          <div class="description">
+            ${place.description}
+          </div>
+        </article>`);
+    }
+  }
+});
